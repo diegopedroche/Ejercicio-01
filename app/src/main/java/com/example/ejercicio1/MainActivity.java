ed54bbdc1d7a4c907a01484f013352c3ccfd809b
Diego Pedroche Molina <Diego@iMac-07.local>
2022-09-20 13:36:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.ejercicio1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnReset;
+    private Button btnSaludar;
+    private TextView lbSaludo;
+    private EditText txtNombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        InicializaVistas();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txtNombre.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "No hay nada escrito en el cuadro de texto", Toast.LENGTH_SHORT).show();
+                }else{
+                    txtNombre.setText("");
+                }
+            }
+        });
+
+        btnSaludar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!txtNombre.getText().toString().equals("")){
+                    lbSaludo.setText("Hola "+txtNombre.getText()+", buenos días");
+                }else{
+                    Toast.makeText(MainActivity.this, "No hay nada escrito en el cuadro de texto", Toast.LENGTH_SHORT).show();
+                    lbSaludo.setText("");
+                }
+            }
+        });
+    }
+
+    private void InicializaVistas() {
+        btnReset = findViewById(R.id.btsResetMain);
+        btnSaludar = findViewById(R.id.btnSaludarMain);
+        txtNombre = findViewById(R.id.txtNombreMain);
+        lbSaludo = findViewById(R.id.lbSaludoMain);
+    }
+}
